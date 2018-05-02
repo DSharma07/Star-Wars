b403a867daf34c6eceb2121220cd64e7feb34f46
DSharma07 <de.jSharma@gmail.com>
2018-05-02 18:20:22 +0100

    Update dashboard.ts

--- src/models/dashboard.ts
+++ src/models/dashboard.ts
@@ -1,6 +1,6 @@
 export const Dashboard = [
   { id: 1, name: 'Films' },
-  { id: 2, name: 'Peoples' },
+  { id: 2, name: 'People' },
   { id: 3, name: 'Starships' },
   { id: 4, name: 'Vehicles' },
   { id: 5, name: 'Species' },
